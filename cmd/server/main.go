7c34326337475fc40b9facd1fa891f25ce8e0e5c
strbnm <strbnm@yandex.ru>
2026-08-22 18:02:36 +0300

    Обновлен main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/strbnm/metrics/internal/handler"
+	"github.com/strbnm/metrics/internal/repository"
+)
+
+func main() {
+	memStorage := repository.NewMemStorage()
+
+	h := handler.NewHandler(memStorage)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /update/{metricType}/{metricName}/{metricValue}/", h.UpdateHandler)
+
+	log.Println("Server is starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
