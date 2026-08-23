5c8e66eff76ee8c899c196274fdba9aa7d104e5c
strbnm <strbnm@yandex.ru>
2026-08-23 19:17:12 +0300

    Добавление роутера для маршрутов

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/strbnm/metrics/internal/handler"
 	"github.com/strbnm/metrics/internal/repository"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -13,9 +15,13 @@ func main() {
 
 	h := handler.NewHandler(memStorage)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("POST /update/{metricType}/{metricName}/{metricValue}/", h.UpdateHandler)
+	r := chi.NewRouter()
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", h.ListAllMetricsHandler)
+		r.Get("/value/{metricType}/{metricName}", h.ValueHandler)
+		r.Post("/update/{metricType}/{metricName}/{metricValue}", h.UpdateHandler)
+	})
 
 	log.Println("Server is starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
