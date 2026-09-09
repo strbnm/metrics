696542c33cf94953c3986926f8afbade64441c0c
strbnm <strbnm@yandex.ru>
2026-09-09 23:09:43 +0300

    Рефакторинг main.go в связи с добавлением логгера и middleware логирования

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-	"log"
+	"errors"
 	"net/http"
 
+	config "github.com/strbnm/metrics/internal/config/server"
 	"github.com/strbnm/metrics/internal/handler"
+	"github.com/strbnm/metrics/internal/logger"
+	"github.com/strbnm/metrics/internal/middleware"
 	"github.com/strbnm/metrics/internal/repository"
 	"github.com/strbnm/metrics/internal/service"
-
-	config "github.com/strbnm/metrics/internal/config/server"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	cleanup := logger.SetupLogger("info")
+	defer cleanup()
+
+	if err := run(); err != nil {
+		logger.Log.Errorf("server error %v", err)
+		panic(err)
+	}
+}
+
+func run() error {
 	cfg, err := config.ParseFlags()
 	if err != nil {
-		log.Fatalf("Configuration parsing failed: %v", err)
+		logger.Log.Errorf("Configuration parsing failed: %v", err)
+		return errors.New("configuration parsing failed")
 	}
 	memStorage := repository.NewMemStorage()
 	metricsService := service.NewMetricsService(memStorage)
@@ -24,12 +36,14 @@ func main() {
 	h := handler.NewHandler(metricsService)
 
 	r := chi.NewRouter()
+	r.Use(middleware.WithLogging)
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", h.ListAllMetricsHandler)
 		r.Get("/value/{metricType}/{metricName}", h.ValueHandler)
 		r.Post("/update/{metricType}/{metricName}/{metricValue}", h.UpdateHandler)
 	})
 
-	log.Printf("Server is starting on %s", cfg.RunAddr)
-	log.Fatal(http.ListenAndServe(cfg.RunAddr, r))
+	logger.Log.Infof("Server is starting on %s", cfg.RunAddr)
+
+	return http.ListenAndServe(cfg.RunAddr, r)
 }
