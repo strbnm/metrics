96eed2d775141a6c53e6fd1047a4f559ba4b3e39
strbnm <strbnm@yandex.ru>
2026-09-10 20:06:04 +0300

    Добавлен кастомный десериализатор для Metrics

--- internal/model/metrics.go
+++ internal/model/metrics.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 const (
 	Counter = "counter"
 	Gauge   = "gauge"
@@ -16,4 +21,45 @@ type Metrics struct {
 	Delta *int64   `json:"delta,omitempty"`
 	Value *float64 `json:"value,omitempty"`
 	Hash  string   `json:"hash,omitempty"`
+}
+
+// Кастомная логика десериализации с валидацией MType
+func (m *Metrics) UnmarshalJSON(data []byte) error {
+	// Аliasing, чтобы избежать рекурсии при вызове json.Unmarshal.
+	type MetricAlias Metrics
+
+	// Парсим во временный объект
+	alias := new(MetricAlias)
+	if err := json.Unmarshal(data, alias); err != nil {
+		return fmt.Errorf("JSON parse error: %w", err)
+	}
+
+	var vErr ValidationError
+
+	// Валидация ID.
+	if alias.ID == "" {
+		vErr.Errors = append(vErr.Errors, FieldError{
+			Field:   "id",
+			Message: "required",
+		})
+	}
+
+	// Валидация MType.
+	switch alias.MType {
+	case Counter, Gauge:
+		// ок
+	default:
+		vErr.Errors = append(vErr.Errors, FieldError{
+			Field:   "type",
+			Message: fmt.Sprintf("invalid value %q, must be %q or %q", alias.MType, Counter, Gauge),
+		})
+	}
+
+	if len(vErr.Errors) > 0 {
+		return &vErr
+	}
+
+	// Только после успешной валидации копируем данные в m.
+	*m = Metrics(*alias)
+	return nil
 }
